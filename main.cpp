f2c9ccc6b84e8c38eefdd6e78bcd2c8edcf3225d
Zain <zainhu06@hotmail.com>
2026-09-25 14:46:07 +0100

    Variable

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
-#include <stdio.h>
+#include <iostream>
+using namespace std;
 
 int main()
 {
-
-    printf("Hello World");
+    int a = 1;
+    printf("Hello World ");
+    cout << a;
     return 0;
 }
